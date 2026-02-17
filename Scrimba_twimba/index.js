52671b7f1ebbb2ba58634ed3afed3d353dac330f
Devanshu Mishra <devmishra2202@gmail.com>
2026-02-17 15:20:58 +0530

    add uuid in data attributes in icons

--- Scrimba_twimba/index.js
+++ Scrimba_twimba/index.js
@@ -10,16 +10,9 @@ function getFeedHtml(){
     let feedHtml = ``
 /*
 Challenge:
-1. Inside each span that has a class of "tweet-detail",
-   add an <i> tag.
-2. Give each <i> tag the classes it needs to render the
-   correct icons next to the numbers.
-   The classes you will need are:
-    fa-regular, 
-    fa-solid, 
-    fa-comment-dots, 
-    fa-heart, 
-    fa-retweet
+1. Add data attributes to each of the  <i> tags. You can call
+   them 'reply', 'like', and 'retweet’.
+2. Each data attribute should hold the tweet's uuid.
 */
     
     tweetsData.forEach(function(tweet){
@@ -31,14 +24,16 @@ Challenge:
             <p class="handle">${tweet.handle}</p>
             <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
-                <span class="tweet-detail"> <i class = " fa-regular fa-comment-dots"></i>
-                    
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
                     ${tweet.replies.length}
                 </span>
-                <span class="tweet-detail"> <i class = " fa-solid fa-heart"></i>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
                     ${tweet.likes}
                 </span>
-                <span class="tweet-detail"> <i class = " fa-solid fa-retweet"></i>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
                     ${tweet.retweets}
                 </span>
             </div>   
