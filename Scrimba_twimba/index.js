0a3dd138396269b83573e6bda1bc98d3b63d1d7d
Devanshu Mishra <devmishra2202@gmail.com>
2026-02-20 12:04:29 +0530

    grab the replies element for show and hide comments

--- Scrimba_twimba/index.js
+++ Scrimba_twimba/index.js
@@ -13,8 +13,11 @@ document.addEventListener('click', function(e){
     else if(e.target.dataset.retweet){
         handleRetweetClick(e.target.dataset.retweet)
     }
+    else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
+    }
 })
-
+ 
 function handleLikeClick(tweetId){ 
     const targetTweetObj = tweetsData.filter(function(tweet){
         return tweet.uuid === tweetId
@@ -45,6 +48,18 @@ function handleRetweetClick(tweetId){
     render() 
 }
 
+function handleReplyClick(replyId){
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+/*
+Challenge:
+1. Use the uuid stored in 'replyId' to take control 
+   of the div containing that tweet’s replies. 
+   (Check the HTML string below to remind yourself 
+   what id that div will have.)  
+2. Toggle the CSS class "hidden" on that div. 
+*/ 
+}
+
 function getFeedHtml(){
     let feedHtml = ``
     
@@ -65,9 +80,9 @@ function getFeedHtml(){
         let repliesHtml = ''
         
         if(tweet.replies.length > 0){
-
             tweet.replies.forEach(function(reply){
-                repliesHtml+= `<div class="tweet-reply">
+                repliesHtml+=`
+<div class="tweet-reply">
     <div class="tweet-inner">
         <img src="${reply.profilePic}" class="profile-pic">
             <div>
@@ -75,25 +90,9 @@ function getFeedHtml(){
                 <p class="tweet-text">${reply.tweetText}</p>
             </div>
         </div>
-</div>`
-            })
-/*
-Challenge:
-1. If a tweet has replies, iterate through the replies
-   and wrap each one in the HTML template provided below. 
-   Make sure to replace words in UPPERCASE with data from 
-   the tweet. On each iteration, add this HTML to repliesHtml.
-   
-<div class="tweet-reply">
-    <div class="tweet-inner">
-        <img src="PROFILE PIC" class="profile-pic">
-            <div>
-                <p class="handle">HANDLE</p>
-                <p class="tweet-text">TWEET TEXT</p>
-            </div>
-        </div>
 </div>
-*/
+`
+            })
         }
         
           
@@ -126,21 +125,11 @@ Challenge:
             </div>   
         </div>            
     </div>
-    <div id="replies-${tweet.uuid}">
-        <!-- REPLIES HERE -->
+    <div class="hidden" id="replies-${tweet.uuid}">
         ${repliesHtml}
-
-
     </div>   
 </div>
 `
-/*
-Challenge:
-2. Place repliesHtml in its parent div remembering 
-   to update that divs id.
-*/
-
-
    })
    return feedHtml 
 }
@@ -150,3 +139,4 @@ function render(){
 }
 
 render()
+
