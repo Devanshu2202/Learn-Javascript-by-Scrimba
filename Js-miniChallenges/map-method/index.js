18c1f6b13625e1d90ca06a63ea2d49621e089f1d
Devanshu Mishra <devmishra2202@gmail.com>
2026-02-21 11:51:46 +0530

    Complete map method challenge

---
+++ Js-miniChallenges/map-method/index.js
@@ -0,0 +1,30 @@
+import { playlistArr } from "./playlist.js";
+
+/*
+Challenge
+1. Refactor the code below to use .map() 
+   instead of the for loop.
+   ⚠️ Don't worry about the commas for now.
+*/
+
+const playlistHtml = [];
+
+playlistArr.map(function (play) {
+  playlistHtml.push(
+    `<section class="card">
+    <div class="card-start">
+        <img src="./images/${play.albumArt}">
+    </div>
+        <div class="card-mid">
+            <h4 class="card-title">${play.title}</h4>
+            <p class="card-artist">${play.artist}</p>
+        </div>
+    <div class="card-end">
+        <p class="card-menu">...</p>
+    </div>
+</section>
+    `,
+  );
+});
+
+document.getElementById("container").innerHTML = playlistHtml;
