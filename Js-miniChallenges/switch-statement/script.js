26c4c37e763907c693ca3fbd1bdd798e285c646f
Devanshu Mishra <devmishra2202@gmail.com>
2026-02-24 12:18:34 +0530

    correct code uppercase and lowercase i the case

--- Js-miniChallenges/switch-statement/script.js
+++ Js-miniChallenges/switch-statement/script.js
@@ -4,35 +4,38 @@
  * Sandwiches $5
  * Salad $4
  * Lemon Cake $3
-*/
+ */
 
 // For example: "You selected Salad. That will be $4"
 
 function selectItem(item) {
-    let price = 0
-/*
+  let price = 0;
+  /*
 Challenge:
 1.  Add the remaining price list items as cases.
-*/    
-    switch(item) {
-        case 'coffee':
-            price = 2
-            break;
-            
-            case 'Sandwiches':
-            price = 5
-            break ;
+*/
+  item = item.toLowerCase();
+  switch (item) {
+    case "coffee":
+      price = 2;
+      break;
 
-             case 'Salad':
-            price = 4
-            break ;
+    case "Sandwiches":
+      price = 5;
+      break;
 
-             case 'SalaLemon Caked':
-            price = 3
-            break ;
-    }
-    
-    return `You selected ${item}. That will be $${price}`
+    case "Salad":
+      price = 4;
+      break;
+
+    case " Lemon Cake":
+      price = 3;
+      break;
+    default:
+      return `sorry we dont sell ${item}`;
+  }
+
+  return `You selected ${item}. That will be $${price}`;
 }
 
-console.log(selectItem('SalaLemon Caked'))
+console.log(selectItem("COFFEE"));
