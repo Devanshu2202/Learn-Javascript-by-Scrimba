7a857533f7bff5c923f6fe52635aa64816032469
Devanshu Mishra <devmishra2202@gmail.com>
2026-03-03 16:48:23 +0530

    using slice method to render 5 posts

--- API/BlogSpace/script.js
+++ API/BlogSpace/script.js
@@ -1,17 +1,25 @@
 /**
- * Challenge:
- * 
- * Send a request to the JSON Placeholder API using fetch
- * URL: https://apis.scrimba.com/jsonplaceholder/posts
- * 
- * Log the response data to the console
+ Challenge:
+
+ Style it up!
+ 
+ * Add a short (~30px height) fixed navbar at the top with the text "BlogSpace". Remember to pad the top of your content so it doesn't get hidden behind the navbar.
+ * Add a font from Google Fonts.
+ * Any other styling you want to make it look nice!
+ 
  */
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-.then((res)=>{
-    return res.json()
-})
-.then((data)=>{
-    console.log(data);
-    
-})
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
+    })
