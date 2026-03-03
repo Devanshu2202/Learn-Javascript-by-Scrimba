8e86471992177c2666dfe2eac3696a17a36a8976
Devanshu Mishra <devmishra2202@gmail.com>
2026-03-03 19:17:09 +0530

    POST API method

--- API/BlogSpace/script.js
+++ API/BlogSpace/script.js
@@ -1,17 +1,17 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-  .then((res) => res.json())
-  .then((data) => {
-    const postsArr = data.slice(0, 5);
-    let html = "";
-    for (let post of postsArr) {
-      html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `;
-    }
-    document.getElementById("blog-list").innerHTML = html;
-  });
+// fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     const postsArr = data.slice(0, 5);
+//     let html = "";
+//     for (let post of postsArr) {
+//       html += `
+//                 <h3>${post.title}</h3>
+//                 <p>${post.body}</p>
+//                 <hr />
+//             `;
+//     }
+//     document.getElementById("blog-list").innerHTML = html;
+//   });
 
 /**
  Challenge:
@@ -41,4 +41,7 @@ myForm.addEventListener("submit", function (e) {
   postTitleInput.value = ""
   postBodyInput.value = ""
 });
+
+
+
 
