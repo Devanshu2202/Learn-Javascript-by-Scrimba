35c8d01482c736ed44b542aa3fa8ae2a39646fd7
Devanshu Mishra <devmishra2202@gmail.com>
2026-03-03 18:26:17 +0530

    get the input value and show as a obj i key value pair

--- API/BlogSpace/script.js
+++ API/BlogSpace/script.js
@@ -1,25 +1,44 @@
-/**
- Challenge:
-
- Style it up!
- 
- * Add a short (~30px height) fixed navbar at the top with the text "BlogSpace". Remember to pad the top of your content so it doesn't get hidden behind the navbar.
- * Add a font from Google Fonts.
- * Any other styling you want to make it look nice!
- 
- */
-
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (let post of postsArr) {
-            html += `
+  .then((res) => res.json())
+  .then((data) => {
+    const postsArr = data.slice(0, 5);
+    let html = "";
+    for (let post of postsArr) {
+      html += `
                 <h3>${post.title}</h3>
                 <p>${post.body}</p>
                 <hr />
-            `
-        }
-        document.getElementById("blog-list").innerHTML = html
-    })
+            `;
+    }
+    document.getElementById("blog-list").innerHTML = html;
+  });
+
+/**
+ Challenge:
+ 
+ * Listen for the "submit" event on the form (which will happen when the button is clicked)
+    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
+ * Combine the title value and body value into an object (with a "title" property and "body" property)
+ * Log the object to the console
+
+*/
+let myForm = document.getElementById("my-form");
+let postTitleInput = document.getElementById("post-title")
+let postBodyInput = document.getElementById("post-body")
+// const postBtn = document.getElementById("post-btn");
+myForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const postTitle = postTitleInput.value
+    const postBody = postBodyInput.value
+
+  const value = {
+    title: postTitle,
+    body: postBody, 
+  };
+  console.log(value);
+
+  postTitleInput.value = ""
+  postBodyInput.value = ""
+});
+
