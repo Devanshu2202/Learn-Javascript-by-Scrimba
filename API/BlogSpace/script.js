de0214c408cdb4e5f072f80b73e0fc0818a4afc6
Devanshu Mishra <devmishra2202@gmail.com>
2026-03-05 12:37:46 +0530

    POST API method

--- API/BlogSpace/script.js
+++ API/BlogSpace/script.js
@@ -1,47 +1,53 @@
-// fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     const postsArr = data.slice(0, 5);
-//     let html = "";
-//     for (let post of postsArr) {
-//       html += `
-//                 <h3>${post.title}</h3>
-//                 <p>${post.body}</p>
-//                 <hr />
-//             `;
-//     }
-//     document.getElementById("blog-list").innerHTML = html;
-//   });
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
+    })
 
-/**
- Challenge:
- 
- * Listen for the "submit" event on the form (which will happen when the button is clicked)
-    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
- * Combine the title value and body value into an object (with a "title" property and "body" property)
- * Log the object to the console
+document.getElementById("my-form").addEventListener("submit", function(e) {
+    e.preventDefault()
+    console.log("vdhdhjwqdjh");
+    
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    const option = {
+      method:"POST",
+      body:JSON.stringify(data),
+      headers:{
+        "Content-Type":"application/json"
+      }
+    }
 
-*/
-let myForm = document.getElementById("my-form");
-let postTitleInput = document.getElementById("post-title")
-let postBodyInput = document.getElementById("post-body")
-// const postBtn = document.getElementById("post-btn");
-myForm.addEventListener("submit", function (e) {
-    e.preventDefault();
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts",option)
+    .then((res)=>{
+      return res.json()
+    })
+    .then((data)=>{
+      console.log(data);
+      
+    })
 
-    const postTitle = postTitleInput.value
-    const postBody = postBodyInput.value
-
-  const value = {
-    title: postTitle,
-    body: postBody, 
-  };
-  console.log(value);
-
-  postTitleInput.value = ""
-  postBodyInput.value = ""
-});
-
-
-
+    /**
+     * Challenge: Send this off to the server!
+     * 
+     * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
+     * 2. Endpoint: /posts
+     * 3. method: ???
+     * 4. Request body: ??? (Remember to turn it into JSON)
+     * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
+     */
+})
 
--- Scrimba-mid-challenges/script.js
+++ Scrimba-mid-challenges/script.js
@@ -1,27 +1,17 @@
-/**
-Challenge:
-
-Send a request to add a new todo item
-    - BaseURL: https://apis.scrimba.com/jsonplaceholder/
-    - Endpoint: /todos
-Body object should contain a "title" property (string) 
-  and a "completed" property (boolean)
-Don't add the .then() blocks yet - we have 1 more thing 
-  to do before it'll work correctly
-
-Part 2 - add the .then blocks to check the data coming back
-
-*/
-
-fetch("https://api.freeapi.app/api/v1/public/randomproducts/1")
-  .then((res) => {
-    console.log("Response received");   // Debug
-    return res.json();
-  })
-  .then((resdata) => {
-    console.log("Full Data:", resdata);
-    console.log("Status Code:", resdata.data.title);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+fetch("https://apis.scrimba.com/jsonplaceholder/todos",{
+  method:"POST",
+  body:JSON.stringify({
+    title: "Buy milk",
+    completed: false
+  }),
+  headers: {
+    "Content-Type": "application/json"
+  }
+})
+.then((res)=>{
+  return res.json()
+})
+.then((data)=>{
+  console.log(data);
+  
+})
