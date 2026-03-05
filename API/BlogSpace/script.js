505fe46e38cb0c011fa1f6b1bff0f9ff6bbddd53
Devanshu Mishra <devmishra2202@gmail.com>
2026-03-05 15:09:21 +0530

    Create render function to show data in UI

--- API/BlogSpace/script.js
+++ API/BlogSpace/script.js
@@ -1,53 +1,55 @@
+let postsArray = []
+
+function renderPosts() {
+  let html = ""
+  for (let post of postsArray) {
+    html += `
+    <h3>${post.title}</h3>
+    <p>${post.body}</p>
+    <hr />
+    `
+  }
+  document.getElementById("blog-list").innerHTML = html
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (let post of postsArr) {
-            html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-        document.getElementById("blog-list").innerHTML = html
+.then(res => res.json())
+.then(data => {
+  
+  postsArray = data.slice(0, 5)
+  
+  renderPosts()
     })
 
 document.getElementById("my-form").addEventListener("submit", function(e) {
     e.preventDefault()
-    console.log("vdhdhjwqdjh");
-    
     const postTitle = document.getElementById("post-title").value
     const postBody = document.getElementById("post-body").value
     const data = {
         title: postTitle,
         body: postBody
     }
-    const option = {
-      method:"POST",
-      body:JSON.stringify(data),
-      headers:{
-        "Content-Type":"application/json"
-      }
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
     }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
 
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts",option)
-    .then((res)=>{
-      return res.json()
-    })
-    .then((data)=>{
-      console.log(data);
-      
-    })
 
-    /**
-     * Challenge: Send this off to the server!
-     * 
-     * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
-     * 2. Endpoint: /posts
-     * 3. method: ???
-     * 4. Request body: ??? (Remember to turn it into JSON)
-     * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
-     */
+          postsArray.unshift(post)
+
+          console.log(post);
+          
+          renderPosts(postsArray)
+           
+        })
 })
+
 
