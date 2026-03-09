9518ee35a394e2d572a805abca079b8f018b8c75
Devanshu Mishra <devmishra2202@gmail.com>
2026-03-09 08:29:52 +0530

    add war project

--- Scrimba-mid-challenges/script.js
+++ Scrimba-mid-challenges/script.js
@@ -1,49 +1,71 @@
-// fetch("https://apis.scrimba.com/jsonplaceholder/todos",{
-//   method:"POST",
-//   body:JSON.stringify({
-//     title: "Buy milk",
-//     completed: false
-//   }),
-//   headers: {
-//     "Content-Type": "application/json"
-//   }
-// })
-// .then((res)=>{
-//   return res.json()
-// })
-// .then((data)=>{
-//   console.log(data);
-  
-// })
+// function handleClick() {
+//     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+//         .then(res => res.json())
+//         .then(data => console.log(data))
+// }
+
+// document.getElementById("new-deck").addEventListener("click", handleClick)
+
+// function callback() {
+//     console.log("I finally ran!")
+// }
+
+// setTimeout(callback, 2000)
+
+// const people = [
+//     { name: "Jack", hasPet: true },
+//     { name: "Jill", hasPet: false },
+//     { name: "Alice", hasPet: true },
+//     { name: "Bob", hasPet: false },
+// ]
+
+// function gimmeThePets(number) {
+//     return person.hasPet
+// }
+
+// const peopleWithPets = people.filter(gimmeThePets)
+// console.log(peopleWithPets)
+
 
 /**
- * Challenge part 1: GET the current weather for your city with 
- * the Open Weather API and log it to the console.
+ * Challenge: 
  * 
- * BaseURL: https://apis.scrimba.com/openweathermap/data/2.5/
- * Endpoint: /weather
- * Query: ??? (https://openweathermap.org/current)
-    * NOTE: It says you need to include appid in your query, but you can skip that this time
+ * Write your own filter function! Don't worry about adding it to the prototype of arrays or anything.
+ * This function should take 2 parameters:
+ * 1. The array you want to filter through, and
+ * 2. A callback function
+ * 
+ * Steps for filterArray function logic:
+ * 1. Initialize a new, empty array which will be returned at the end of the filterArrays operations (Completed ✅)
+ * 2. Loop through the array passed as the 1st parameter
+ * 3. Inside the loop, call the callback function, passing the individual item you're currently looping over as the argument to your callback function
+ * 4. If the callback function returns true, push the current item you're iterating on in the loop to the new array. If it returns false, don't push it to the array.
+ * 5. When the loop is over, return the new array
  */
 
-fetch("https://apis.scrimba.com/openweathermap/data/2.5/weather?q=Ahmedabad")
-.then((res)=>{
-  return res.json()
-})
-.then((data)=>{
-  console.log(data);
-  
-})
+const people = [
+    { name: "Jack", hasPet: true },
+    { name: "Jill", hasPet: false },
+    { name: "Alice", hasPet: true },
+    { name: "Bob", hasPet: false },
+]
 
- /**
- * Challenge: Define our anonymous callback function as a separate function, then
- * pass it as the 2nd parameter to our addEventListener
- */
+function filterArray(array, callback) {
+    const resultingArray = []
 
- let shuffle = function (){
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-        .then(res => res.json())
-        .then(data => console.log(data))
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            resultingArray.push(array[i])
+        }
+    }
+
+    return resultingArray
 }
 
-document.getElementById("deck-btn").addEventListener("click", shuffle)
+function hasPet(person) {
+    return person.hasPet
+}
+
+const peopleWithPets = filterArray(people, hasPet)
+
+console.log(peopleWithPets)
