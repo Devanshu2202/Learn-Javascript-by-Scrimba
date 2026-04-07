f29d6bb64334cb253b0e3e9220fe2e504e8173ed
Devanshu Mishra <devmishra2202@gmail.com>
2026-04-07 06:02:22 +0530

    fetch API data in useeffect to render random images

--- React-Basics/meme_generator/src/componenets/MemeGenerator.jsx
+++ React-Basics/meme_generator/src/componenets/MemeGenerator.jsx
@@ -1,11 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const MemeGenerator = () => {
-  const [topText, setTopText] = useState("");
-  const [bottomText, setBottomText] = useState("Walk into Mordor");
+  const [allmeme, setAllMeme] = useState([]);
+  const [memeText, setMemeText] = useState({
+    topText: "",
+    bottomText: "Walk into Mordor",
+    image:
+      "https://wompimages.ampify.care/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F001%2F179%2F956%2F8f1.jpg",
+  });
 
-  const memeImage =
-    "https://wompimages.ampify.care/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F001%2F179%2F956%2F8f1.jpg";
+  // console.log("memeText", memeText);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMeme(data.data.memes));
+  }, []);
+  //generate random number
+
+  // const memeImage =
+  //   "https://wompimages.ampify.care/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F001%2F179%2F956%2F8f1.jpg";
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    // console.log("name", name);
+    // console.log("value", value);
+
+    setMemeText((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+  function getImage() {
+    if (allmeme.length === 0) return;
+    const randomIndex = Math.floor(Math.random() * allmeme.length);
+
+    console.log("randomIndex", randomIndex);
+    const randomMeme = allmeme[randomIndex].url;
+
+    setMemeText((prev) => ({
+      ...prev,
+      image: randomMeme,
+    }));
+  }
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center">
@@ -19,34 +57,39 @@ const MemeGenerator = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <input
             type="text"
+            name="topText"
             placeholder="Top Text"
-            value={topText}
-            onChange={(e) => setTopText(e.target.value)}
+            value={memeText.topText}
+            onChange={handleChange}
             className="border rounded-md p-3 w-full"
           />
 
           <input
             type="text"
+            name="bottomText"
             placeholder="Bottom Text"
-            value={bottomText}
-            onChange={(e) => setBottomText(e.target.value)}
+            value={memeText.bottomText}
+            onChange={handleChange}
             className="border rounded-md p-3 w-full"
           />
         </div>
 
-        <button className="w-full mt-4 bg-purple-700 text-white py-2 rounded-md hover:bg-purple-800 transition">
+        <button
+          onClick={getImage}
+          className="w-full mt-4 bg-purple-700 text-white py-2 rounded-md hover:bg-purple-800 transition"
+        >
           Get a new meme image
         </button>
 
         <div className="relative mt-6">
-          <img src={memeImage} alt="meme" className="w-full rounded-lg" />
+          <img src={memeText.image} alt="meme" className="w-full rounded-lg" />
 
           <h2 className="absolute top-2 left-1/2 transform -translate-x-1/2 text-white text-lg md:text-2xl font-bold uppercase text-center drop-shadow-lg">
-            {topText}
+            {memeText.topText}
           </h2>
 
           <h2 className="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-white text-lg md:text-2xl font-bold uppercase text-center drop-shadow-lg">
-            {bottomText}
+            {memeText.bottomText}
           </h2>
         </div>
       </div>
